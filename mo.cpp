a968b97581863e91455a097e3d3600cc3e614d79
hartmanm <16545449+hartmanm@users.noreply.github.com>
2020-12-06 12:41:54 -0500

    Update mo.cpp

--- mo.cpp
+++ mo.cpp
@@ -4,7 +4,8 @@
 #include <fstream>
 #include <iterator>
 #include <ctime>
-#include <map>
+//#include <map>
+//#include <algorithm>
 using namespace std;
 
 
@@ -37,7 +38,7 @@ int main(int argv, char *argc[]){
 string current_dtg=get_dtg();
 cout << current_dtg;
 
-return 0;
+//return 0;
 
 
 
@@ -77,12 +78,11 @@ system(cmd);
 to_file.open ("regen");
 to_file << file_string;
 to_file.close();
-
 system("cat regen");
 
 
 
-
+file_string="";
 
 
 
@@ -117,61 +117,126 @@ std::cout<<"word 'mars' not found"<<std::endl;
 
 
 
+ifstream _the_file("datafile");
+string _file_string(istreambuf_iterator<char>{_the_file}, {});
+
+// file_string
 
 
-const char* cstring_file = file_string.c_str();
-const char* ifs = ",";
-const char* line_separator = "\n";
+// file_string
+
+//const char* cstring_file = _file_string.c_str();
+
+//int the_size=sizeof(_cstring_file);
+//const char* cstring_file[the_size] = _cstring_file;
+//char* ifs = "\t";
+//char* line_separator = "\n";
 int iterator=0;
 const char* char_iterator;
-char* line;
-char* token;
+
+//char line[500];
+//char token[100];
+//string line;
+string token;
+//char processed[10000];
 string processed;
 
 
-
-
-bool in_token=true;
-bool column_header_mode=false;
+//bool in_token=true;
+//bool column_header_mode=false;
 //bool on_header=true;
 
-map<string, string> kvstore;
+//map<string, string> kvstore;
+//vector<string>
 
-while(cstring_file[iterator] != EOF){
-char_iterator=&cstring_file[iterator];
-if(char_iterator == line_separator){
+/*
+time_t now = time(0);
+char* cdate = ctime(&now);
+char _no_spaces_date[24];
+string date=string(cdate);
+string no_spaces_date;
+int _iterator=0;
+while(_iterator < sizeof(date)){
+if(date[_iterator] != ' ' && date[_iterator] != ':'){_no_spaces_date[_iterator]=date[_iterator];}
+if(date[_iterator] == ' ' || date[_iterator] == ':'){_no_spaces_date[_iterator]='_';}
+//cout << endl;
+//cout << date[_iterator];
+//cout << endl;
+//cout << _no_spaces_date;
+_iterator++;
+}
+no_spaces_date=string(_no_spaces_date);
+return no_spaces_date;
+*/
+string cstring_file = _file_string;
+cout << endl << cstring_file;
 
-line=0;
+//for_each(_file_string.begin(), _file_string.end(), fn);
+
+while(iterator < cstring_file.length()){
+//char_iterator=cstring_file[iterator];
+cout << endl;
+cout << cstring_file[iterator];
+
+if(cstring_file[iterator] == '\n' && token == ""){
+
+//line="";
+processed+='\n';
+cout << "processed newline";
 }
 
 
-if(char_iterator == ifs){
-if(column_header_mode){kvstore.insert(make_pair("earth",token));}
 
-kvstore.insert(make_pair("earth",token));
+if(cstring_file[iterator] == ' ' || (cstring_file[iterator] == '\n' && token != "")){
+cout << "processed space";
 
-
-in_token=false;
-}
-
-
-if(char_iterator != ifs && char_iterator != line_separator){
-if(in_token){
-
+processed+=token;
+if(cstring_file[iterator] == '\n' && token != ""){processed+='\n';}
+if(cstring_file[iterator] == ' '){processed+=' ';}
+cout << "processed" << processed << endl;
+cout << "token" << token << endl;
+token="";
 
 }
+
+/*
+//if(cstring_file[iterator] == 't' && cstring_file[iterator-1] == '\\'){
+if(cstring_file[iterator] == ' '){
+//if(column_header_mode){kvstore.insert(make_pair("earth",token));}
+cout << "inside space";
+//kvstore.insert(make_pair("earth",token));
+cout << "processed space";
+processed+=token;
+processed+=' ';
+token=nullptr;
+cout << endl;
+//cout << token;
+//cout << "processed space";
+}
+*/
+
+if(cstring_file[iterator] != ' ' && cstring_file[iterator] != '\n'){
+//if(in_token){
+
+token+=cstring_file[iterator];
+//}
 
 }
 
 //if(){}
 //if(){}
-
 iterator++;
 }
 
 
 
+cout << endl << processed;
 
+// write in memory string to file
+to_file.open ("processed");
+to_file << processed;
+to_file.close();
+system("cat processed");
 
 return 0;
 }
